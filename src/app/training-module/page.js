d9e638a56126e1088f9a7fb2f8450d631542434b
KhoaPhung22 <khoaphung22@gmail.com>
2025-06-14 11:47:17 -0300

    Creating the link to the module 1 in the trainning module

--- src/app/training-module/page.js
+++ src/app/training-module/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+
 const Module = ({ title, subtitle, bgColor, borderColor, href }) => (
   <Link href={href} passHref>
     <div className={`p-4 ${bgColor} ${borderColor} border-4 mb-4 cursor-pointer hover:bg-opacity-90`}>
@@ -15,13 +16,13 @@ const TrainingModule = () => (
     <p className="text-green-700 text-center mb-4">
       Engage with interactive scenarios and group discussion designed to enhance critical thinking and decision-making.
     </p>
-    <Module title="MODULE 1: What Is Sustainability" bgColor="bg-green-100" borderColor="border-blue-500" href="/modules/module1"/>
-    <Module title="MODULE 2: Dimensions of Sustainability" subtitle="Environmental, economic, technical and social sustainability" bgColor="bg-yellow-100" borderColor="border-blue-500" href="/module" />
-    <Module title="MODULE 3: Social Sustainability" bgColor="bg-green-100" borderColor="border-blue-500" href="/module3" />
-    <Module title="MODULE 4: Dimensions of Social Sustainability" bgColor="bg-yellow-100" borderColor="" />
-    <Module title="MODULE 5: Dimensions of Sustainability" subtitle="Environmental, economic, technical and social sustainability" bgColor="bg-green-100" borderColor="" />
-    <Module title="MODULE 6: Social Sustainability" bgColor="bg-yellow-100" borderColor="" />
-    <Module title="MODULE 7: module 7: Implementation Strategies : Practical approaches to implementing sustainable software practices " bgColor="bg-green-100" borderColor="" />
+    <Module title="MODULE 1: What Is Sustainability" bgColor="bg-green-100" borderColor="border-blue-500" href="/modules/module1" />
+    <Module title="MODULE 2: Dimensions of Sustainability" subtitle="Environmental, economic, technical and social sustainability" bgColor="bg-yellow-100" borderColor="border-blue-500" href="/modules/module2" />
+    <Module title="MODULE 3: Social Sustainability" bgColor="bg-green-100" borderColor="border-blue-500" href="/modules/module3" />
+    <Module title="MODULE 4: Dimensions of Social Sustainability" bgColor="bg-yellow-100" borderColor="" href="/modules/module4" />
+    <Module title="MODULE 5: Dimensions of Sustainability" subtitle="Environmental, economic, technical and social sustainability" bgColor="bg-green-100" borderColor="" href="/modules/module5" />
+    <Module title="MODULE 6: Social Sustainability" bgColor="bg-yellow-100" borderColor="" href="/modules/module6" />
+    <Module title="MODULE 7: Implementation Strategies : Practical approaches to implementing sustainable software practices" bgColor="bg-green-100" borderColor="" href="/modules/module7" />
   </div>
 );
 
