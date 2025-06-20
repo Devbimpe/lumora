1389211953c7e0c067dc03575a9737f772667471
vbharti <vs336320@dal.ca>
2025-06-20 01:32:08 -0300

    resolve merge

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,17 @@
-import Footer from './Footer'
-import './globals.css'
-import './header.css'
 import { Header } from './components/Header';
+import Footer from './Footer';
+import './globals.css';
+import './header.css';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-<<<<<<< HEAD
-      <body>{children}
+      <body>
+        <Header />
+        {children}
         <Footer />
       </body>
       
     </html>
   )
-=======
-    <body>
-        <Header />
-        {children}
-    </body>
-    </html>
-  );
->>>>>>> cdbe4ff17828808fa314322cf324f8b536888966
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-export default function Page() {
-  return <h1 className="text-3xl font-bold underline">This is the "Root" of our project</h1>
-}
-
-=======
 import CarouselSection from "./components/carousel-section"
+import Footer from "./components/footer"
 import HowItWorksSection from "./components/how-it-works-section"
 import TrainingModulesSection from "./components/training-modules-section"
-import Footer from "./components/footer"
 
 export default function HomePage() {
   return (
@@ -19,4 +13,3 @@ export default function HomePage() {
     </div>
   )
 }
->>>>>>> cdbe4ff17828808fa314322cf324f8b536888966
