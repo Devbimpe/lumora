7bab25d1151c26c428b8223bf13e83d7f4b0f33c
junjiel <jn281687@dal.ca>
2025-07-04 12:25:24 -0300

    admin front end done

---
+++ src/app/admin/module-management/page.js
@@ -0,0 +1,9 @@
+export default function UserManagement() {
+  return (
+    <div className="text-center text-black text-xl w-full">
+      <h2 className="font-bold">Module Management</h2>
+      <p>module management here.</p>
+      {/* Add user management specific content */}
+    </div>
+  );
+}
---
+++ src/app/admin/settings/page.js
@@ -0,0 +1,9 @@
+export default function UserManagement() {
+  return (
+    <div className="text-center text-black text-xl w-full">
+      <h2 className="font-bold">Settings</h2>
+      <p>settings here.</p>
+      {/* Add user management specific content */}
+    </div>
+  );
+}
