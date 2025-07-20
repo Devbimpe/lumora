3eb121a0e3c9103f98a55719d85dafb47aef2fae
KhoaPhung22 <khoaphung22@gmail.com>
2025-07-20 14:44:32 -0300

    Remove the or statment

--- src/app/api/content/route.js
+++ src/app/api/content/route.js
@@ -5,11 +5,11 @@ import mysql from 'mysql2/promise';
 async function getDbConnection() {
   try {
     const connection = await mysql.createConnection({
-      host: process.env.DB_HOST || 'localhost',
-      user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME || 'your_database_name',
-      port: process.env.DB_PORT || 3306,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
     });
     return connection;
   } catch (error) {
