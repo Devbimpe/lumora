a70513267c719956df8345dad4f770dc3eafe2d2
vbharti <vs336320@dal.ca>
2025-07-21 12:44:22 -0300

    final db changes done

--- db/LUMORA_db.sql
+++ db/LUMORA_db.sql
@@ -15,7 +15,7 @@ CREATE TABLE Users(
 CREATE TABLE Modules(
     ModuleID INT PRIMARY KEY auto_increment,
     Heading varchar(50) NOT NULL,
-    Subheading TEXT(100) NOT NULL
+    Subheading varchar(104) NOT NULL
 );
 
 CREATE TABLE Content(
@@ -59,22 +59,26 @@ INSERT INTO Content (ModuleID, Overview, Reading) VALUES
 (1, "overview", "Software systems are now foundational to nearly every aspect of how we live, work, and connect as a society. In fact, software doesn’t just support our world, it shapes it. As our dependency on digital systems grows, so does our responsibility to ensure that these systems are designed and maintained with long-term well-being in mind.The growing scale and influence of software brings with it both extraordinary potential and significant pressure. If not built with care, digital systems can cause long-term harm socially, environmentally, economically, and even technically.This module will introduce you to the concept of sustainability in software engineering, explain why it matters, and lay the foundation for the dimensions that will be explored in later modules"),
 (1, "What is Sustainability in Software Engineering?", "Sustainability, in a general sense, is the ability to meet current needs without compromising the ability of future generations to meet their own. Sustainability in software is not just about energy efficiency; it's about building systems that can adapt, endure, and remain responsible over time. It means designing software that continues to serve users and communities, whether that’s through more maintainable code, more inclusive design, or more mindful resource use.In software engineering, this means creating systems that are:● Durable: able to evolve with technological change● Responsible: considerate of social and environmental consequences● Maintainable: built in ways that support long-term usability and cost-efficiency“Sustainable software is software that meets the needs of the present without compromising the ability of future generations to meet their own needs.” Adapted from the Brundtland Report (1987)."),
 (1, "Why Sustainability Matters in Software", "Software is not neutral. Every system built affects people, infrastructure, and the planet. Unsustainable software systems often lead to:● High technical debt and short software lifespan● Inequitable access to technolog● Social or algorithmic harm (e.g., bias, exclusion, privacy or safety risks)● Excessive energy consumption and environmental wasteThe more society relies on software, the more critical it becomes that we engineer systems that are resilient, ethical, and future-ready."),
-(1, "Common Misconceptions", "Misconception Sustainability is just about the environment or “Green IT” Sustainable systems are less efficient Only architects or leaders should worry about this
-worry about this Every developer, designer, and tester should contribute to sustainability decisions");
-
+(1, "Common Misconceptions", "Sustainability is just about the environment or “Green IT” "),
+(1, "Correction", "In software engineering, it also includes social, technical, and economic endurance."),
+(1, "Common Misconceptions", "Sustainable systems are less efficient"),
+(1, "Correction", "In fact, they often increase efficiency through maintainability."),
+(1, "Common Misconceptions", "Only architects or leaders should worry about this"),
+(1, "Correction","Every developer, designer, and tester should contribute to sustainability decision"),
+(1, "Knowledge Check", "A. Ensuring software runs as fast and efficiently as possible to meet user expectations B. Designing systems that can adapt and endure over time while minimizing negative social, environmental, technical, and economic impacts C. Reducing development costs by limiting scope and cutting unnecessary features D. Building digital systems that prioritize environmental goals above all else");
 
 
 
 -- Insert into KnowledgeChecks
 INSERT INTO KnowledgeChecks (ContentID, Question, Answer) VALUES
-(1, "Which of the following best captures the meaning of sustainability in software engineering?", "B");
+(10, "Which of the following best captures the meaning of sustainability in software engineering?", "B");
 
 -- Insert into StudentSubmissions
 INSERT INTO StudentSubmissions (KnowledgeCheckID, StudentID, SubmissionAnswer, Grade) VALUES
 (1, 1, "JSX is a syntax for React.", 90.00),
-(1, 1, "State changes, props don't.", 85.00),
-(1, 2, "Server-side rendering is rendering on the server.", NULL),
-(1, 2, "API routes are in pages/api folder.", 95.00);
+(2, 1, "State changes, props don't.", 85.00),
+(3, 2, "Server-side rendering is rendering on the server.", NULL),
+(4, 2, "API routes are in pages/api folder.", 95.00);
 
 
 -- Update User table for the email activation flow
@@ -92,6 +96,7 @@ SELECT * FROM Content;
 SELECT * FROM KnowledgeChecks;
 SELECT * FROM StudentSubmissions;
 SELECT * FROM modulecontent;
+
 
 
 
