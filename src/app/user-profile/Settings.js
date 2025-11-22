d3f9810a7de0887cf1d1de8e52e812841509cd9f
Harmin Patel <hr373866@dal.ca>
2025-11-22 18:18:33 -0400

    feat: Add Delete Account button to Settings Container

--- src/app/user-profile/Settings.js
+++ src/app/user-profile/Settings.js
@@ -1,6 +1,11 @@
 "use client";
+import { useState } from "react";
+import { Trash2 } from "lucide-react";
 
 export default function Settings({ userId }) {
+  const [status, setStatus] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  
   return (
     <div className="bg-white p-6 text-left rounded-xl shadow-sm border border-gray-200">
       <p className="text-xl text-green-700 mb-2 font-bold">Account Settings</p>
@@ -12,7 +17,11 @@ export default function Settings({ userId }) {
           <p className="text-red-700 text-lg font-semibold">Delete Account</p>
           <p className="text-red-600 text-md">Permanently delete your account and all associated data.</p>
         </div>
+        <button onClick={() => setShowModal(true)} className="bg-red-600 text-white px-8 py-2 rounded-lg flex items-center gap-2 hover:bg-red-700 transition"><Trash2 size={18} /> Delete</button>
       </div>
+
+      {status && (<p className={`mt-2 text-sm text-center ${ status.includes("Error") ? "text-red-600" : "text-green-700"}`}>{status}</p>)}
+
     </div>
   );
 }
