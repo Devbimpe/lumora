f5799ce0411f67c8faef4c476502af089c22c4c5
Ma'ayan <my923139@dal.ca>
2026-07-16 16:11:51 -0300

    remove home button from landing page

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -60,12 +60,7 @@ export function Header() {
 
           {/* Desktop Navigation */}
           <div className="hidden md:flex items-center gap-6 lg:gap-8">
-            <Link 
-              href="/#top" 
-              className="text-gray-700 hover:text-green-600 font-medium transition-colors"
-            >
-              Home
-            </Link>
+            
 
             {user && ( 
               <Link 
